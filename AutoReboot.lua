e983abdf7cdd1b299b9122f4e8565e7c12fb6b2a
Colin <colinkinzel@gmail.com>
2017-03-28 10:00:30 -0500

    Update AutoReboot.lua

--- AutoReboot.lua
+++ AutoReboot.lua
@@ -2,7 +2,12 @@ local component = require("component")
 local c = require("computer")
 
 
-local delay = 1
---how long before next check
+local delay = 300
+--how long before next Reboot
 
 while true do 
+  if c.uptime() >= delay then
+    c.beep(1000, 2)
+    c.sleep(45)
+    computer.shutdown(true)
+    end
