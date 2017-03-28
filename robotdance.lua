f9620f02ec3919269a5bcfcde6528c940ba0adb7
Colin <colinkinzel@gmail.com>
2017-03-28 10:32:12 -0500

    Most of the code is finished

---
+++ robotdance.lua
@@ -0,0 +1,35 @@
+local r = require("robot")
+local c = require("computer")
+local running = true
+local dance_number = 0
+
+while running do
+	dance_number = math.random(10)
+	
+	if dance_number == 1 then
+	r.forward()
+	elseif dance_number == 2 then
+	r.turnAround()
+	elseif dance_number == 3 then
+	r.up()
+	elseif dance_number == 4 then
+	r.turnLeft()
+	elseif dance_number == 5 then
+	r.back()
+	elseif dance_number == 6 then
+	r.turnRight()
+	elseif dance_number == 7 then
+	r.down()
+	elseif dance_number == 8 then
+	--Custom dance number
+	r.up()
+	r.down()
+	r.turnLeft()
+	elseif dance_number == 9 then
+	--Custom dance number
+	
+	elseif dance_number == 10 then
+	--Custom dance number
+	
+	
+	end
