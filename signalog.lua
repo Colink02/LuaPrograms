e052ee39638c383c50aaeca93d9c5b92e135d919
Colin <colinkinzel@gmail.com>
2017-03-29 20:41:51 -0500

    Update signalog.lua

--- signalog.lua
+++ signalog.lua
@@ -1,6 +1,11 @@
+local component = require("component")
 local c = require("computer")
 local event = require("event")
+local m = component.modem
+ m.open(123)
+
 while true do
+
  local _, remoteAddress, port, payload = event.pull("modem_message")
  file = io.open("event.txt", "a")
  io.output(file)
