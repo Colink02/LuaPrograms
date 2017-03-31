9d0ad195b0ea8f186d89737028c0842f758ff49c
Colin <colinkinzel@gmail.com>
2017-03-30 22:12:18 -0500

    Update service.lua

--- service.lua
+++ service.lua
@@ -2,11 +2,22 @@ local c = require("computer")
 local com = require("component")
 local t = com.tunnel
 local r = com.br_reactor
-local is_running = false
+local last_message = nil
 local tmp = " "
 local message = "temp"
+local timer_id = nil
 
-function start()
+local function service_callback() 
+local next = r.getactive()
+  if last_message ~= next then
+    t.send(next) 
+    end
+  last_message = next
+end
+    
+    
+    
+function start() if timer_id then
 is_running = true
   while start do
     if message == tmp then
