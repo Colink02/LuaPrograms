5eaaae8ed05fd3a2e0e1fbff9da8f1f6caf7b33a
Colin <colinkinzel@gmail.com>
2017-03-31 15:53:35 -0500

    Update servicelog.lua

--- servicelog.lua
+++ servicelog.lua
@@ -2,11 +2,11 @@ local c = require("computer")
 local com = require("component")
 local event = require("event")
 local t = com.tunnel
-local file = log.txt
+local file = "log.txt"
+io.open(file, a)
+io.output(file)
 while true do 
   local message = event.pull("modem_message")
   print(message)
-  io.open(file, a)
   io.write(message.."\n")
-  io.close(file)
 end
