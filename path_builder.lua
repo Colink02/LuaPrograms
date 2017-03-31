58989f1a821e26cc8fa7505b7d2956a65886c585
Colin <colinkinzel@gmail.com>
2017-03-31 14:04:01 -0500

    Almost complete

--- path_builder.lua
+++ path_builder.lua
@@ -1,8 +1,29 @@
 local c = require("computer")
 local r = require("robot")
 local com = require("component")
-local inv = com.
+print("what file would you like to open?")
+file = io.read()
+io.open(file)
+io.input(file)
+local size = io.read (1)
+
 while true do
+  local line = io.read("*number")
+  if io.read() == "f" then
+    r.moveforward
+  elseif io.read() == "tl" then
+    r.turnLeft()
+  elseif io.read() == "tr" then
+    r.turnRight()
+  elseif io.read() == "d" then
+    r.down()
+  elseif io.read() == "u" then
+    r.up()
+  elseif io.read() == "b" then
+    r.back()
+  elseif io.read() == "invselect" then
+    r.select(inv)
+  else
+    print("Invaild Code!")
+    end
   
-  r.placeDown()
-  r
