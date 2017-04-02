6f221d69befe2def29013e239cec56afd801ddcf
Colin <colinkinzel@gmail.com>
2017-04-02 11:52:09 -0500

    Update Printer.lua

--- Printer.lua
+++ Printer.lua
@@ -4,6 +4,11 @@ local 3d = component.printer3d
 local LightLevel = 3d.getlightlevel
 local buttonMode = 3d.isButtonMode()
 
+print("what file will this save to?")
+local file = io.read()
+io.open(file, a)
+io.output(file)
+
 function readinput()
 print("type help for a list of commands")
 input = io.read()
@@ -13,10 +18,12 @@ if input == "lightlevel" then
   print("Block light level will be"..LightLevel)
 end
 if input == "button on" then
+    if buttonMode == "
   print("Turning on Block Button Mode")
   io.write("setButtonMode(true)")
 end
 if input == "button off" then 
+  
   
 
 readinput()
