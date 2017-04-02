f7e39230afcf7985c672069dc1836388f0b95d15
Colin <colinkinzel@gmail.com>
2017-04-02 09:46:38 -0500

    Create basecontrol.lua

---
+++ basecontrol.lua
@@ -0,0 +1,11 @@
+local c = require("computer")
+local com = require("component")
+local rs = com.redstone
+
+
+--Config
+local side1 = "5" --For RFTools Shield
+local side2 = "1" --For Power Disable and Enable
+--End of Config
+
+
